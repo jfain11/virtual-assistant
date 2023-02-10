cd6b8ca62a87c3fad42126f9389cd08bc4ae5bf0
Jacob Fain <jacobharryfain@gmail.com>
2023-02-09 22:42:40 -0500

    Added microphone recording capability
    Completed basic voice recognition
    
    Currently just prints a transcription of what was said

--- VoiceRecognition.py
+++ VoiceRecognition.py
@@ -1,3 +1,34 @@
 # Controls the voice recognition for the virtual assistant.
 
 import speech_recognition as sr
+
+r = sr.Recognizer()
+
+
+
+mic = sr.Microphone()
+
+
+input("press when ready")
+with mic as source:
+    audio = r.listen(source)
+
+
+
+response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+}
+try:
+    response["transcription"] = r.recognize_google(audio)
+except sr.RequestError:
+    # API was unreachable or unresponsive
+    response["success"] = False
+    response["error"] = "API unavailable"
+
+except sr.UnknownValueError:
+        # speech was unintelligible
+        response["error"] = "Unable to recognize speech"
+
+print(response)
